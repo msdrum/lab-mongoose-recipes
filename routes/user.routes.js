a20eeea4e578638ba8202870e2ea036ae523cb8d
Marcio Silva <marciosilvadrum@gmail.com>
2022-12-01 15:09:40 -0300

    user get by id, done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,37 @@
 import express from "express";
 import UserModel from "../models/User.model.js";
+import RecipeModel from "../models/Recipe.model.js";
 
 const userRoute = express.Router();
 
 //ROUTES
 
-//CREAT USER AND SHOW JUST THE USERNAME
+//CREAT A NEW USER
 userRoute.post("/create", async (req, res) => {
   try {
     const form = req.body;
 
     const newUser = await UserModel.create(form);
 
-    return res.status(201).json(newUser.username);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
+// GET BY ID
+userRoute.get("/read/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await UserModel.findById(userId).populate("recipes");
+
+    if (!user) {
+      return res.status(400).json({ msg: " User not found" });
+    }
+
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.status(500).json(error.errors);
